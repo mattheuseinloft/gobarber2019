ea592408a7204675be39698f430554bad352143f
Mattheus Einloft <mattheuseinloft@gmail.com>
2019-10-25 20:00:23 -0300

    avatar + appointments + notifications + emails

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'c583ebe08642a7',
+    pass: '401cdfb423d56c',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
